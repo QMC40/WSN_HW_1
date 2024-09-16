f6324dc7a897402d8885fab3c76a9d57cd9e9aa1
Aaron Fortner <afortner@islander.tamucc.edu>
2024-09-15 22:27:29 -0500

    final turn in vers

--- main.py
+++ main.py
@@ -1,42 +1,42 @@
 """
     File: main.py
     Author: Aaron Fortner
-    Date: 09/8/2024
-    Version: 0.4
+    Date: 09/15/2024
+    Version: 0.5
 
     Description: This file contains the main code for the Wireless Sensor Network (WSN) simulation.
 
     See the main README.md file for detailed information, installation instructions, and usage.
 """
 import logging
+import os
 import random
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import KDTree
 from tabulate import tabulate
 
-# Constant values
+# Constants
 # Logging switch
-LOGGING = True
+LOGGING = False
 # Max limits of coordinates
 MAX_COORD = 20
 # Dimensional length of cluster sides in meters
 CLUSTER_SIZE = 5
 # Number of clusters in the network
 NUM_CLUSTERS = 16
-# Clusters index at 0 or 1? Set to match zero element index
-CLUSTER_INDEX = 0
-# Nodes index at 0 or 1? Set to match zero element index
-NODE_INDEX = 0
-
+# Clusters index at 0 or 1 for output and display? Set to match zero element index numbering
+CLUSTER_INDEX = 1
+# Nodes index at 0 or 1 for output and display? Set to match zero element index numbering
+NODE_INDEX = 1
 
 # Configure logging
 logger = logging.getLogger('WSN')  # Create a logger for the WSN code
-logger.setLevel(logging.DEBUG)  # Set the desired logging level for your code
+logger.setLevel(logging.DEBUG)  # Set the desired logging level to DEBUG
 
 # Set up the logging configuration for the external libraries
 logging.getLogger('matplotlib').setLevel(logging.WARNING)  # Suppress DEBUG logs from matplotlib
-logging.getLogger('scipy').setLevel(logging.WARNING)  # Suppress logs from scipy if needed
+logging.getLogger('scipy').setLevel(logging.WARNING)  # Suppress DEBUG logs from scipy
 
 # Set up logging format
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -50,7 +50,7 @@ console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
 if not LOGGING:
-    logging.disable(logging.CRITICAL)  # Disable logging to the console
+    logging.disable(logging.CRITICAL)  # Disable logging to the console if LOGGING is False
 
 
 class Node:
@@ -63,7 +63,8 @@ class Node:
     The fitness value (f) is calculated as follows:     f = 0.4 * r + 0.4 * e + 0.2 * p
     where r is the communication range, e is the energy level, and p is the processing power.
 
-    The node can also be assigned to a cluster and used to calculate the distance to another node.
+    The node is also assigned to a cluster and the node class includes the ability to  calculate the distance to
+    another node.
     """
 
     def __init__(self, node_id: int, x: float, y: float, r: int, e: int, p: int):
@@ -176,7 +177,8 @@ class Cluster:
         col = self.cluster_id % 4
         row = self.cluster_id // 4
         center_position = np.array(
-                [(col * CLUSTER_SIZE) + CLUSTER_SIZE / 2, (row * CLUSTER_SIZE) + CLUSTER_SIZE / 2]
+                [(col * CLUSTER_SIZE) + CLUSTER_SIZE / 2,
+                 (row * CLUSTER_SIZE) + CLUSTER_SIZE / 2]
                 )
         return Node(-1, float(center_position[0]), float(center_position[1]), 0, 0, 0)
 
@@ -211,8 +213,7 @@ class WSN:
         """
         if self.mode == 'random':
             # Pick random number of nodes between 10 and 100
-            # num_nodes = random.randint(10, 100)
-            num_nodes = 10
+            num_nodes = random.randint(10, 100)
             # Generate random nodes with coordinates, communication range, energy level, and processing power.
             for i in range(num_nodes):
                 while True:
@@ -222,10 +223,10 @@ class WSN:
 
                     # check for duplication of existing node at those coords, regen if found, otherwise finish
                     # construction of candidate node and save to the network
-                    dupe_node = not any(node.position[0] == x and node.position[1] == y for node in self.nodes)
+                    not_dupe = not any(node.position[0] == x and node.position[1] == y for node in self.nodes)
 
-                    if dupe_node:
-                        # If no duplicate found, add the new node
+                    if not_dupe:
+                        # If no duplicate found, generate the remaining characteristics and add the new node
                         r = random.randint(1, 8)
                         e = random.randint(1, 100)
                         p = random.randint(1, 100)
@@ -237,19 +238,25 @@ class WSN:
                         print(f"Node @ coordinates ({x}, {y}) already exists. Regenerating...")
 
         elif self.mode == 'user':
+
+            # Check if the input file exists
+            if not os.path.exists(self.input_file):
+                print(f"Error: The input file '{self.input_file}' does not exist.")
+                return  # Exit the function if the file doesn't exist
+
             # Read nodes from the input file in user mode
             with open(self.input_file, 'r') as file:
                 num_nodes = int(file.readline().strip())
                 for i in range(num_nodes):
-                    x, y, r, e, p = map(float, file.readline().strip().split())
+                    x, y, r, e, p = map(int, file.readline().strip().split())
                     # check for duplication of existing node at those coords, reject if found, otherwise finish
                     # construction of candidate node and save to the network
                     # Check for duplication of existing node at those coordinates
-                    dupe_node = not any(node.position[0] == x and node.position[1] == y for node in self.nodes)
+                    not_dupe = not any(node.position[0] == x and node.position[1] == y for node in self.nodes)
 
-                    if dupe_node:
-                        # If no duplicate found, add the new node, otherwise skip the node. R, E, and P are cast to int
-                        self.nodes.append(Node(i, x, y, int(r), int(e), int(p)))
+                    if not_dupe:
+                        # If no duplicate found, add the new node, otherwise skip the node.
+                        self.nodes.append(Node(i, x, y, r, e, p))
                         logger.debug(f"Node {i} @ ({x}, {y}) with R={r}, E={e}, P={p} added to the network.")
                     else:
                         print(f"Node @ coordinates ({x}, {y}) already exists in user input. Skipping...")
@@ -260,7 +267,7 @@ class WSN:
         """
         # Iterate through all nodes and assign them to the appropriate cluster based on their coordinates.
         for node in self.nodes:
-            cluster_id = get_cluster_id(node.position[0], node.position[1])
+            cluster_id = get_cluster_id(node.position[0], node.position[1], node.node_id)
             self.clusters[cluster_id].add_node(node)
 
     def elect_clusterheads(self) -> None:
@@ -296,8 +303,8 @@ class WSN:
             return []
 
         # Get the source and destination nodes
-        src_node = self.get_node_by_id(src_id - NODE_INDEX)
-        dest_node = self.get_node_by_id(dest_id - NODE_INDEX)
+        src_node = self.get_node_by_id(src_id)
+        dest_node = self.get_node_by_id(dest_id)
 
         # Start the route with the source node
         route = [src_node]
@@ -311,8 +318,7 @@ class WSN:
             # Get the nearest node to the destination within the current node's transmission range
             next_node = self.get_nearest_node_kdtree(current_node, dest_node, already_visited)
 
-            if not next_node:
-                # if not next_node or next_node.node_id in visited:
+            if not next_node or next_node.node_id in already_visited:
                 print("No valid route found.")
                 return []  # Return an empty route if no valid next node is found or if stuck in a loop
 
@@ -338,7 +344,8 @@ class WSN:
         radius = current_node.r
         neighbors_within_range = self.kd_tree.query_ball_point(current_node.position, r=radius)
 
-        # Check if the destination node is within the current node's transmission range
+        # Check if the destination node is within the current node's transmission range, if so finish the route and
+        # return the destination node as the next node in the route.
         if dest_node.node_id in neighbors_within_range:
             logger.debug(f"Destination node {dest_node.node_id} is within range of node {current_node.node_id}.")
             return dest_node
@@ -399,21 +406,43 @@ class WSN:
                 if cluster.nodes:
                     file.write(f'\nCluster {cluster.cluster_id + CLUSTER_INDEX}:\n')
 
-                    node_ids = ", ".join(str(node.node_id) for node in cluster.nodes)
+                    node_ids = ", ".join(str(node.node_id + NODE_INDEX) for node in cluster.nodes)
                     file.write(f'Nodes: {node_ids}\n')
 
                     if cluster.clusterhead:
-                        file.write(f'Clusterhead: {cluster.clusterhead.node_id}\n')
+                        file.write(f'Clusterhead: {cluster.clusterhead.node_id + NODE_INDEX}\n')
 
 
-def get_cluster_id(x: int, y: int) -> int:
+def get_cluster_id(x: int, y: int, node_num: int) -> int:
     """
     Get the cluster ID based on the coordinates of a node.
 
     :param x: X-coordinate of the node
     :param y: Y-coordinate of the node
+    :param node_num: node number for logging
     :return cluster_id: Cluster ID based on the coordinates
     """
+
+    def rank_and_file_calculator(value: int):
+        """
+        Calculates the correct row or column of the network grid of a node
+        :param value: the relevant coordinate for the node being located
+        :return: the row or column within which the node resides
+        """
+        # Calculate the row and column of the node based on the node's coordinates by floor division of the coordinate
+        # by the cluster length.
+        # Then subtract the result of the floor division of the coord by the max coord value
+        # to reflect nodes on the outer edge back inside the network grid.
+        result = int((value // CLUSTER_SIZE) - (value // MAX_COORD))
+
+        # If the node is on the border between two segments, randomly assign it to one of the two adjacent segments.
+        # If the node is at the outside edge of the network, keep the segment the same.
+        # The adjustment is made by subtracting one from the result.
+        if value % CLUSTER_SIZE == 0 and value != 0 and value != 20:
+            if random.choice([True, False]):
+                result -= 1
+        return result
+
     # Call rank and file calc to determine the row and col containing the node
     col = rank_and_file_calculator(x)
     row = rank_and_file_calculator(y)
@@ -424,29 +453,8 @@ def get_cluster_id(x: int, y: int) -> int:
     # so if the node is in row 0, col 0, and indexed at 0 the cluster ID is 0 (0 * 4 + 0 + 0), if its in row 3,
     # col 1, and indexed at 1 the cluster ID is 14 (3 * 4 + 1 + 1).
     cluster = row * 4 + col
-    logger.debug(f"node @ {x},{y} : {col},{row} assigned to cluster {cluster}")
+    logger.debug(f"node {node_num} @ {x},{y} : {col},{row} assigned to cluster {cluster}")
     return cluster
-
-
-def rank_and_file_calculator(value: int):
-    """
-    Calculates the correct row or column of the network grid of a node
-    :param value: the relevant coordinate for the node being located
-    :return: the row or column within which the node resides
-    """
-    # Calculate the row and column of the node based on the node's coordinates by floor division of the coordinate
-    # by the cluster length.
-    # Then subtract the result of the floor division of the coord by the max coord value
-    # to reflect nodes on the outer edge back inside the network grid.
-    result = int((value // CLUSTER_SIZE) - (value // MAX_COORD))
-
-    # If the node is on the border between two segments, randomly assign it to one of the two adjacent segments.
-    # If the node is at the outside edge of the network, keep the segment the same.
-    # The adjustment is made by subtracting one from the result.
-    if value % CLUSTER_SIZE == 0 and value != 0 and value != 20:
-        if random.choice([True, False]):
-            result -= 1
-    return result
 
 
 def display_network_info(wsn):
@@ -479,8 +487,8 @@ def display_network_info(wsn):
     cluster_data = []
     for cluster in wsn.clusters:
         if cluster.nodes:
-            cluster_nodes = ", ".join(str(node.node_id) for node in cluster.nodes)
-            clusterhead = cluster.clusterhead.node_id if cluster.clusterhead else "None"
+            cluster_nodes = ", ".join(str(node.node_id + NODE_INDEX) for node in cluster.nodes)
+            clusterhead = cluster.clusterhead.node_id + NODE_INDEX if cluster.clusterhead else "None"
             cluster_data.append(
                     [
                             cluster.cluster_id + CLUSTER_INDEX,
@@ -531,7 +539,7 @@ def plot_nodes(nodes, clusters, connect=False, title='WSN Nodes and Their Radio 
         ax.add_patch(circle)
 
         # Annotate the node with its ID
-        ax.text(node.position[0] + 0.3, node.position[1], f'ID: {node.node_id}', fontsize=9)
+        ax.text(node.position[0] + 0.3, node.position[1], f'ID: {node.node_id + NODE_INDEX}', fontsize=9)
 
         # Collect coordinates if connecting nodes
         if connect:
@@ -605,33 +613,37 @@ def main() -> None:
                 src_id = None
                 dest_id = None
 
-                # Validate source node ID
+                # Validate source node ID, note the subtraction of the NODE_INDEX to the input to align displayed node
+                # IDs with the displayed node_ids based on the 1 or 0 indexing setting.
                 while src_id is None:
                     try:
                         src_id = int(input("\nEnter source node ID: "))
-                        if wsn.get_node_by_id(src_id) is None:
-                            print(f"Source node ID {src_id} is not valid. Please enter a valid node ID.")
+                        if wsn.get_node_by_id(src_id - NODE_INDEX) is None:
+                            print(f"The network does not contain a node  with the ID:{src_id}. Please "
+                                  f"enter a valid node ID.")
                             src_id = None
                     except ValueError:
                         print("Invalid input. Please enter a valid integer for the node ID.")
                         src_id = None
 
-                # Validate destination node ID
+                # Validate destination node ID, note the subtraction of the NODE_INDEX to the input to align displayed
+                # node IDs with the displayed node_ids based on the 1 or 0 indexing setting.
                 while dest_id is None:
                     try:
                         dest_id = int(input("Enter destination node ID: "))
-                        if wsn.get_node_by_id(dest_id) is None:
-                            print(f"Destination node ID {dest_id} is not valid. Please enter a valid node ID.")
+                        if wsn.get_node_by_id(dest_id - NODE_INDEX) is None:
+                            print(f"The network does not contain a node  with the ID:{dest_id}. Please "
+                                  f"enter a valid node ID.")
                             dest_id = None
                     except ValueError:
                         print("Invalid input. Please enter a valid integer for the node ID.")
                         dest_id = None
 
                 # Call the find_route method and print the route if one exists
-                route = wsn.find_route(src_id, dest_id)
+                route = wsn.find_route(src_id - NODE_INDEX, dest_id - NODE_INDEX)
                 # Print the route if it exists and ask the user if they want to route another packet or quit
                 if route:
-                    print(f'Route: {" -> ".join(map(str, route))}')
+                    print(f'Route: {" -> ".join(f"{node + NODE_INDEX}" for node in route)}')
                     if plot:
                         # Plot the nodes and their radio ranges with the route connected
                         plot_nodes(
--- test_main.py
+++ test_main.py
@@ -1,3 +1,13 @@
+"""
+    File: test_main.py
+    Author: Aaron Fortner
+    Date: 09/8/2024
+    Version: 0.4
+
+    Description: This file contains the unit testing code for the Wireless Sensor Network (WSN) simulation.
+
+    See the main README.md file for detailed information, installation instructions, and usage.
+"""
 import unittest
 import numpy as np
 from main import Node, Cluster, WSN, get_cluster_id
@@ -7,20 +17,48 @@ class TestWSN(unittest.TestCase):
 
     def setUp(self):
         """Create a basic WSN for testing."""
-        self.wsn = WSN(mode='random')
+        self.wsn = WSN(mode='user', input_file='testing/single_input.txt')
+        self.wsn.nodes.clear()
+        self.wsn.kd_tree = None
+
+    def test_wsn_setup(self):
+        """Test that the WSN is set up correctly."""
         # Create nodes manually for predictable behavior
-        node1 = Node(1, 0, 0, 5, 50, 50)  # (x, y, range, energy, processing power)
-        node2 = Node(2, 3, 4, 5, 50, 50)  # Within range of node1
-        node3 = Node(3, 6, 8, 5, 50, 50)  # Within range of node2
-        self.wsn.nodes = [node1, node2, node3]
+        node0 = Node(0, 0, 0, 5, 50, 50)  # (x, y, range, energy, processing power)
+        node1 = Node(1, 3, 4, 5, 50, 50)  # Within range of node1
+        node2 = Node(2, 6, 8, 5, 50, 50)  # Within range of node2
+
+        self.wsn.nodes = [node0, node1, node2]
         self.wsn.build_kd_tree()
+
+        self.assertEqual(len(self.wsn.nodes), 3)
+        self.assertEqual(len(self.wsn.clusters), 16)
+        self.assertEqual(self.wsn.kd_tree.data.shape, (3, 2))
+        self.assertEqual(self.wsn.nodes[0].node_id, 0)
+        self.assertEqual(self.wsn.nodes[1].node_id, 1)
+        self.assertEqual(self.wsn.nodes[2].node_id, 2)
+        self.assertEqual(self.wsn.nodes[0].position[0], 0)
+        self.assertEqual(self.wsn.nodes[0].position[1], 0)
+        self.assertEqual(self.wsn.nodes[1].position[0], 3)
+        self.assertEqual(self.wsn.nodes[1].position[1], 4)
+        self.assertEqual(self.wsn.nodes[2].position[0], 6)
+        self.assertEqual(self.wsn.nodes[2].position[1], 8)
+        self.assertEqual(self.wsn.nodes[0].r, 5)
+        self.assertEqual(self.wsn.nodes[1].r, 5)
+        self.assertEqual(self.wsn.nodes[2].r, 5)
+        self.assertEqual(self.wsn.nodes[0].e, 50)
+        self.assertEqual(self.wsn.nodes[1].e, 50)
+        self.assertEqual(self.wsn.nodes[2].e, 50)
+        self.assertEqual(self.wsn.nodes[0].p, 50)
+        self.assertEqual(self.wsn.nodes[1].p, 50)
+        self.assertEqual(self.wsn.nodes[2].p, 50)
 
     def test_node_generation(self):
         """Test that nodes are generated in random mode."""
-        wsn = WSN(mode='random')
-        self.assertGreaterEqual(len(wsn.nodes), 10)
-        self.assertLessEqual(len(wsn.nodes), 100)
-        for node in wsn.nodes:
+        self.wsn = WSN(mode='random')
+        self.assertGreaterEqual(len(self.wsn.nodes), 10)
+        self.assertLessEqual(len(self.wsn.nodes), 100)
+        for node in self.wsn.nodes:
             self.assertGreaterEqual(node.position[0], 0)
             self.assertLessEqual(node.position[0], 20)
             self.assertGreaterEqual(node.position[1], 0)
@@ -28,65 +66,75 @@ class TestWSN(unittest.TestCase):
 
     def test_node_distance(self):
         """Test the Euclidean distance between two nodes."""
-        node1 = Node(1, 0, 0, 5, 50, 50)
-        node2 = Node(2, 3, 4, 5, 50, 50)  # Distance should be 5 (3-4-5 triangle)
-        distance = node1.distance_to(node2)
-        self.assertEqual(distance, 5)
+        node0 = Node(0, 0, 0, 5, 50, 50)
+        node1 = Node(1, 3, 4, 5, 50, 50)  # Distance should be 5 (3-4-5 triangle)
+        node2 = Node(2, 6, 8, 5, 50, 50)
+        node3 = Node(3, 9, 12, 5, 50, 50)
+        node4 = Node(4, 8, 0, 5, 50, 50)
+        node5 = Node(5, 3, 15, 5, 50, 50)
+        distance1 = node0.distance_to(node1)
+        distance2 = node0.distance_to(node2)
+        distance3 = node0.distance_to(node3)
+        distance4 = node0.distance_to(node4)
+        distance5 = round(node0.distance_to(node5), 8)
+        distance6 = node1.distance_to(node5)
+        self.assertEqual(distance1, 5)
+        self.assertEqual(distance2, 10)
+        self.assertEqual(distance3, 15)
+        self.assertEqual(distance4, 8)
+        self.assertEqual(distance5, 15.29705854)
+        self.assertEqual(distance6, 11)
 
     def test_cluster_assignment(self):
         """Test that nodes are correctly assigned to clusters based on their coordinates."""
         self.wsn.assign_clusters()
         for node in self.wsn.nodes:
-            cluster_id = get_cluster_id(node.position[0], node.position[1])
+            cluster_id = get_cluster_id(node.position[0], node.position[1], node.node_id)
             self.assertIn(node, self.wsn.clusters[cluster_id].nodes)
 
     def test_cluster_head_election(self):
         """Test that the correct node is elected as the cluster head."""
-        cluster = Cluster(1)
-        node1 = Node(1, 5, 5, 4, 70, 40)  # F = 0.4 * 4 + 0.4 * 70 + 0.2 * 40 = 33.6
-        node2 = Node(2, 6, 6, 3, 80, 50)  # F = 0.4 * 3 + 0.4 * 80 + 0.2 * 50 = 38.2
-        node3 = Node(3, 7, 7, 2, 60, 20)  # F = 0.4 * 2 + 0.4 * 60 + 0.2 * 20 = 26.8
+        cluster = Cluster(5)
+        node0 = Node(0, 5, 5, 4, 70, 40)  # F = 0.4 * 4 + 0.4 * 70 + 0.2 * 40 = 37.6
+        node1 = Node(1, 6, 6, 3, 80, 50)  # F = 0.4 * 3 + 0.4 * 80 + 0.2 * 50 = 43.2
+        node2 = Node(2, 7, 7, 2, 60, 20)  # F = 0.4 * 2 + 0.4 * 60 + 0.2 * 20 = 28.8
+        node3 = Node(3, 5, 9, 3, 80, 50)  # F = 0.4 * 3 + 0.4 * 80 + 0.2 * 50 = 43.2
+        cluster.add_node(node0)
         cluster.add_node(node1)
         cluster.add_node(node2)
         cluster.add_node(node3)
         cluster.elect_clusterhead()
-        self.assertEqual(cluster.clusterhead.node_id, 2)
+        print(f'Clusterhead before adding node 5: {cluster.clusterhead.node_id}')
+        self.assertEqual(cluster.clusterhead.node_id, 1)
+        node4 = Node(4, 9, 9, 3, 80, 50)  # F = 0.4 * 3 + 0.4 * 80 + 0.2 * 50 = 43.2
+        cluster.add_node(node4)
+        cluster.elect_clusterhead()
+        print(f'Clusterhead: {cluster.clusterhead.node_id}')
+        self.assertTrue(cluster.clusterhead.node_id in [1, 4])
 
-    # def test_routing(self):
-    #     """Test that a route is found between nodes when one exists."""
-    #     wsn = WSN(mode='random')
-    #     # Create a scenario where nodes can be routed
-    #     node1 = Node(1, 0, 0, 5, 50, 50)
-    #     node2 = Node(2, 3, 4, 5, 50, 50)
-    #     node3 = Node(3, 6, 8, 5, 50, 50)
-    #     wsn.nodes = [node1, node2, node3]
-    #     wsn.assign_clusters()
-    #     route = wsn.find_route(1, 3)
-    #     self.assertEqual(route, [1, 2, 3])
+    def test_routing(self):
+        """Test that a route is found between nodes when one exists."""
+        # Create a scenario where nodes can be routed
+        node0 = Node(0, 0, 0, 5, 50, 50)
+        node1 = Node(1, 3, 4, 5, 50, 50)
+        node2 = Node(2, 6, 8, 5, 50, 50)
+        self.wsn.nodes = [node0, node1, node2]
+        self.wsn.assign_clusters()
+        self.wsn.build_kd_tree()
+        self.wsn.elect_clusterheads()
+        route = self.wsn.find_route(0, 2)
+        self.assertEqual(route, [0, 1, 2])
 
-    # def test_invalid_routing(self):
-    #     """Test that no route is found when nodes are out of range."""
-    #     wsn = WSN(mode='random')
-    #     node1 = Node(1, 0, 0, 2, 50, 50)  # Range too small to connect
-    #     node2 = Node(2, 10, 10, 2, 50, 50)
-    #     wsn.nodes = [node1, node2]
-    #     wsn.assign_clusters()
-    #     route = wsn.find_route(1, 2)
-    #     self.assertEqual(route, [])
-
-    # def test_valid_route(self):
-    #     """Test that a valid route is found."""
-    #     route = self.wsn.find_route(1, 3)
-    #     self.assertEqual(route, [1, 2, 3])
-
-    # def test_no_route(self):
-    #     """Test that no route is found if nodes are out of range."""
-    #     node1 = Node(1, 0, 0, 2, 50, 50)  # Small range, can't reach node2
-    #     node2 = Node(2, 10, 10, 2, 50, 50)
-    #     self.wsn.nodes = [node1, node2]
-    #     self.wsn.build_kd_tree()
-    #     route = self.wsn.find_route(1, 2)
-    #     self.assertEqual(route, [])
+    def test_invalid_routing(self):
+        """Test that no route is found when nodes are out of range."""
+        node0 = Node(0, 0, 0, 2, 50, 50)  # Range too small to connect
+        node1 = Node(1, 10, 10, 2, 50, 50)
+        self.wsn.nodes = [node0, node1]
+        self.wsn.assign_clusters()
+        self.wsn.build_kd_tree()
+        self.wsn.elect_clusterheads()
+        route = self.wsn.find_route(0, 1)
+        self.assertEqual(route, [])
 
 
 if __name__ == "__main__":
